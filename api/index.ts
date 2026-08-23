020fa99fd3bae26df76dbe9630c41b8b5136b6d1
Yash Jakasaniya <yashjaka01@gmail.com>
2026-08-23 17:23:51 +0530

    Fix api/index.ts TS2349 type error by exporting Express app directly for Vercel serverless functions

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,3 @@
 import app from "../artifacts/api-server/src/app";
 
-export default function handler(req: any, res: any) {
-  if (req.url && !req.url.startsWith("/api")) {
-    req.url = "/api" + req.url;
-  }
-  return app(req, res);
-}
+export default app;
