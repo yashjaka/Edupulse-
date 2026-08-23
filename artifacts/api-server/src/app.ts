bca14d036a2cc7f73c78ae64d2b94ee81ef0153f
Yash Jakasaniya <yashjaka01@gmail.com>
2026-08-23 17:51:57 +0530

    Replace complex pino-http middleware with clean native Express logging middleware in app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,31 +1,14 @@
 import express, { type Request, type Response, type NextFunction } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
-import type { IncomingMessage, ServerResponse } from "http";
 import router from "./routes/index.js";
 import { logger } from "./lib/logger.js";
 
 const app = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req: IncomingMessage & { id?: unknown }) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res: ServerResponse) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  }),
-);
+app.use((req: Request, _res: Response, next: NextFunction) => {
+  logger.info({ method: req.method, url: req.url?.split("?")[0] }, "request");
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
